d4278eeb8ec703f08693d189f255d6603e2becbf
sugrob69 <sugrob.boid@gmail.com>
2018-01-20 22:19:08 +0300

    remove file

--- start.js
+++ start.js
@@ -3,9 +3,36 @@ let config = {};
 
 fs.readFile('./config.json', 'utf-8', (err, data) => {
     if (err) {
-        new Error(err);
+        throw new Error(err);
     }
 
     config = JSON.parse(data);
-    console.log(config);
+
+    fs.readdir(config.path, 'utf-8', (err, data) => {
+        if (err) {
+            throw new Error(err);
+        }
+
+        data.forEach((file, index, arr) => {
+            let pathfile = config.path + "/" + file;
+            fs.stat(pathfile, (err, stats) => {
+                if (err) {
+                    throw new Error(err);
+                }
+
+                if (stats.isFile()) {
+                    let regex = new RegExp("\." + config.files + "$");
+                    if (regex.test(file)) {
+                        fs.unlink(pathfile, (err) => {
+                            if (err) {
+                                throw new Error(err);
+                            }
+
+                            console.log(`Remove ${pathfile}`);
+                        })
+                    }
+                }
+            });
+        });
+    });
 });
