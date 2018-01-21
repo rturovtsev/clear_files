a4a725300c5a94341539af6518feaee5c23a8701
sugrob69 <sugrob.boid@gmail.com>
2018-01-21 15:09:09 +0300

    require json

--- start.js
+++ start.js
@@ -1,38 +1,33 @@
 const fs = require('fs');
-let config = {};
+const config = require('./config');
 
-fs.readFile('./config.json', 'utf-8', (err, data) => {
+
+fs.readdir(config.path, 'utf-8', (err, data) => {
     if (err) {
         throw new Error(err);
     }
 
-    config = JSON.parse(data);
+    data.forEach((file, index, arr) => {
+        let pathfile = config.path + "/" + file;
 
-    fs.readdir(config.path, 'utf-8', (err, data) => {
-        if (err) {
-            throw new Error(err);
-        }
+        fs.stat(pathfile, (err, stats) => {
+            if (err) {
+                throw new Error(err);
+            }
 
-        data.forEach((file, index, arr) => {
-            let pathfile = config.path + "/" + file;
-            fs.stat(pathfile, (err, stats) => {
-                if (err) {
-                    throw new Error(err);
+            if (stats.isFile()) {
+                let regex = new RegExp(`\.${config.files}$`);
+                
+                if (regex.test(file)) {
+                    fs.unlink(pathfile, (err) => {
+                        if (err) {
+                            throw new Error(err);
+                        }
+
+                        console.log(`Remove ${pathfile}`);
+                    })
                 }
-
-                if (stats.isFile()) {
-                    let regex = new RegExp(`\.${config.files}$`);
-                    if (regex.test(file)) {
-                        fs.unlink(pathfile, (err) => {
-                            if (err) {
-                                throw new Error(err);
-                            }
-
-                            console.log(`Remove ${pathfile}`);
-                        })
-                    }
-                }
-            });
+            }
         });
     });
 });
