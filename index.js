aeed51494fab065d236c706952782f60d0db3e91
sugrob69 <sugrob.boid@gmail.com>
2018-01-22 21:44:31 +0300

    async

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const fs = require('fs');
 const config = require('./config');
+const async = require('async');
+const path = config.path;
+const fileExt = config.files;
 
-
-fs.readdir(config.path, 'utf-8', (err, data) => {
-    if (err) {
-        throw new Error(err);
-    }
-
-    data.forEach((file, index, arr) => {
-        let pathfile = config.path + "/" + file;
-
-        fs.stat(pathfile, (err, stats) => {
-            if (err) {
-                throw new Error(err);
-            }
-
-            if (stats.isFile()) {
-                let regex = new RegExp(`\.${config.files}$`);
-
-                if (regex.test(file)) {
-                    fs.unlink(pathfile, (err) => {
-                        if (err) {
-                            throw new Error(err);
-                        }
-
-                        console.log(`Remove ${pathfile}`);
-                    })
-                }
-            }
+async.waterfall([
+    function readData(callback) {
+        fs.readdir(path, 'utf-8', (err, files) => {
+            callback(err, files);
         });
-    });
+    },
+    function loopFile(files, callback) {
+        files.forEach((file) => {
+            callback(null, file);
+        });
+    },
+    function checkFile(file, callback) {
+        let pathfile = path + file;
+
+        fs.stat(pathfile, function (err, stats) {
+            callback(err, stats, file, pathfile);
+        });
+    },
+    function readData(stats, file, pathfile, callback) {
+        if (stats.isFile()) {
+            let regex = new RegExp(`\.${fileExt}$`);
+
+            if (regex.test(file)) {
+                fs.unlink(pathfile, (err) => {
+                    callback(err,`Remove ${pathfile}`);
+                })
+            } else {
+                callback(null, "Files not found");
+            }
+        }
+    }
+], (err, result) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log(result);
+    }
 });
