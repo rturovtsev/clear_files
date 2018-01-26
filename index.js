02661f54bfedfa206c50835e5fbc62eee7eccca0
sugrob69 <sugrob.boid@gmail.com>
2018-01-26 18:29:15 +0300

    learn async

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 const fs = require('fs');
 const config = require('./config');
-const async = require('async');
 const path = config.path;
 const fileExt = config.files;
 
-async.waterfall([
-    function readData(callback) {
+async function makeCleaner() {
+    const files = await readData();
+    await loopFile(files);
+}
+
+makeCleaner().catch(console.error).then(console.log("Done"));
+
+function readData() {
+    return new Promise((resolve, reject) => {
         fs.readdir(path, 'utf-8', (err, files) => {
-            callback(err, files);
+            if (err) reject(new Error(err.message));
+            resolve(files);
         });
-    },
-    function loopFile(files, callback) {
-        files.forEach((file) => {
-            callback(null, file);
-        });
-    },
-    function checkFile(file, callback) {
-        let pathfile = path + file;
+    });
+}
 
-        fs.stat(pathfile, function (err, stats) {
-            callback(err, stats, file, pathfile);
-        });
-    },
-    function readData(stats, file, pathfile, callback) {
-        if (stats.isFile()) {
-            let regex = new RegExp(`\.${fileExt}$`);
+function loopFile(arr = []) {
+    return new Promise((resolve, reject) => {
+        arr.forEach((file) => {
+            let pathfile = path + file;
 
-            if (regex.test(file)) {
-                fs.unlink(pathfile, (err) => {
-                    callback(err,`Remove ${pathfile}`);
-                })
-            } else {
-                callback(null, "Files not found");
-            }
-        }
-    }
-], (err, result) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        console.log(result);
-    }
-});
+            fs.stat(pathfile, function (err, stats) {
+                if (err) reject(new Error(err.message));
+
+                if (stats.isFile()) {
+                    let regex = new RegExp(`\.${fileExt}$`);
+
+                    if (regex.test(file)) {
+                        fs.unlink(pathfile, (err) => {
+                            if (err) reject(new Error(err.message));
+                            console.log(`Remove ${pathfile}`);
+                        })
+                    }
+                }
+            });
+        });
+    });
+}
